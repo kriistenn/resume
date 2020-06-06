9449533abf4a9b1b7fa612ef6cf700f4526f225a
kriistenn <christinadefault@gmail.com>
2020-06-06 13:28:55 +0300

    contact-list styles adapt

--- src/components/ContactList/Card.js
+++ src/components/ContactList/Card.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import styles from './card.module.css'
 
 
 function Card( {contact} ) {
     return (
-        <NavLink
+        <div 
+        className={styles.card}
         style={{display: 'block'}}
         to = {{
             pathname: `/portfolio/contact-list/${contact.id}`,
-            state: {name: contact.name, phone: contact.phone}
+            state: {name: contact.name, phone: contact.phone, addres: contact.addres}
         }}
         key={contact.id}
         >
-        {contact.name}
-        &nbsp;
-        {contact.phone}
-        </NavLink>
+      <div>     
+       <div className={styles.name}> Name:{contact.name}</div>
+       <div className={styles.phone}> Phone: {contact.phone}</div>
+       <div className={styles.phone}> Addres: {contact.addres}</div>
+      </div>
+        </div>
     );
 }
 
--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -7,8 +7,10 @@ function ContactList() {
     const initialState = {
         contacts: [],
         name: '',
-        phone: ''
+        phone: '',
+        addres: ''
     }
+
     
     const reducer = (state, action) => {
         switch (action.type) {
@@ -16,36 +18,43 @@ function ContactList() {
                 return {...state, name: action.payload };
             case 'CHANGE_PHONE':
                 return {...state, phone: action.payload };
+            case 'CHANGE_ADDRES':
+                return {...state, addres: action.payload };
             case 'ADD_NEW_CONTACT':
                 return {
                     ...state,
                     name: '',
                     phone: '',
-                    contacts: [...state.contacts, action.payload] };
+                    addres: '',
+                    contacts: [...state.contacts, action.payload] 
+                };
             default:
             throw new Error();
         }
     }
     
+
+
     const [state, dispatch] = React.useReducer(reducer, initialState)
-    const { name, phone, contacts } = state
+    const { name, phone, addres, contacts } = state
     
     
     const handleAdd = (e) => {
         e.preventDefault()
-        if (!name || !phone) {
+        if (!name || !phone || !addres) {
             alert('заполните все поля')
             return
         }
-        const newContact = { id: shortid.generate(), name, phone }
+        const newContact = { id: shortid.generate(), name, phone, addres }
 
         dispatch({ type: 'ADD_NEW_CONTACT', payload: newContact })
     }
     
         return (
+        <div className={style.container}>
         <div className={style.page}>
             <form className={style.form}>
-            <h3>Новый контакт</h3>
+            <span className={style['form-title']}>New contact</span>
             <input 
                 type="text" 
                 placeholder="Имя" 
@@ -58,13 +67,22 @@ function ContactList() {
                 value={phone}
                 onChange={(e) => dispatch({ type: 'CHANGE_PHONE', payload: e.target.value })}
             />
-            <button onClick={handleAdd} >
+            <input 
+                type="text" 
+                placeholder="Адрес" 
+                value={addres}
+                onChange={(e) => dispatch({ type: 'CHANGE_ADDRES', payload: e.target.value })}
+            />
+           <div className={style.btn}>
+           <button className={style.btnAdd} onClick={handleAdd} >
             Добавить
             </button>
+           </div>
             </form>
             <div className={style.list}>
-                { contacts.map(c => <Card key={c.id} contact={c}/>) }
+                { contacts.map(c => <Card  key={c.id} contact={c}/>) }
             </div>
+        </div>
         </div>
     )
 }
