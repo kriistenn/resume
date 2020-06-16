c65aa9a7736a6073dfd22bf93ad6fc5cfa3f53a8
kriistenn <christinadefault@gmail.com>
2020-06-16 15:24:20 +0300

    Updates

---
+++ precache-manifest.9c737a49438b9f92dc5ee202ad800c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b21479226f793f5de775e2906bb94eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12454a231472719ca6f6",
+    "url": "/static/css/main.6546765e.chunk.css"
+  },
+  {
+    "revision": "88424b2e715bc0eceecf",
+    "url": "/static/js/2.3fc9f931.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3fc9f931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12454a231472719ca6f6",
+    "url": "/static/js/main.34aae8ef.chunk.js"
+  },
+  {
+    "revision": "34a23f0ee4a1ee7d0d48",
+    "url": "/static/js/runtime-main.e5b1187f.js"
+  }
+]);
